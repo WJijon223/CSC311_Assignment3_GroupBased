16a7c2edfecc5165b83e92e9cd0a2971bb1f2cf1
WJijon223 <williamjijon223@gmail.com>
2025-02-23 12:20:58 -0500

    Added reset, solve, car, and robot buttons. Also created functions for each button (have yet to be implemented)

--- src/main/java/edu/farmingdale/csc311_assignment3_groupbased/MainController.java
+++ src/main/java/edu/farmingdale/csc311_assignment3_groupbased/MainController.java
@@ -1,14 +1,42 @@
 package edu.farmingdale.csc311_assignment3_groupbased;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private RadioButton carButton;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private RadioButton robotButton;
+
+    @FXML
+    private Button solveButton;
+
+    @FXML
+    void resetMaze(ActionEvent event) {
+
     }
+
+    @FXML
+    void setCar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setRobot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void solveMaze(ActionEvent event) {
+
+    }
+
 }
