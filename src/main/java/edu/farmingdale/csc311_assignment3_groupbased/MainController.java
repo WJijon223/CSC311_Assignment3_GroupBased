e4e98b2b12db3804316bde3abf85067ea4953db4
WJijon223 <williamjijon223@gmail.com>
2025-02-22 23:40:05 -0500

    changed fxml and java file names

--- src/main/java/edu/farmingdale/csc311_assignment3_groupbased/MainController.java
+++ src/main/java/edu/farmingdale/csc311_assignment3_groupbased/MainController.java
@@ -3,7 +3,7 @@ package edu.farmingdale.csc311_assignment3_groupbased;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
