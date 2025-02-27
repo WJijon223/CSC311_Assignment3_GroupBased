6bce0eae0189a9883795409ab5d303ef4b7c4da7
Marie <goudm@farmingdale.edu>
2025-02-27 11:04:24 -0500

    added car class

--- src/main/java/edu/farmingdale/csc311_assignment3_groupbased/Car.java
+++ src/main/java/edu/farmingdale/csc311_assignment3_groupbased/Car.java
@@ -2,11 +2,13 @@ package edu.farmingdale.csc311_assignment3_groupbased;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.transform.Rotate;
 
 public class Car implements Movable {
     private double x, y;
     private final double width, height;
     private final double speed;
+    private double angle = 0; // Rotation angle
 
     public Car(double x, double y) {
         this.x = x;
@@ -17,21 +19,44 @@ public class Car implements Movable {
     }
 
     public void draw(GraphicsContext gc) {
+        gc.save();
+        rotate(gc, angle, x + width / 2, y + height / 2);
         gc.setFill(Color.RED);
         gc.fillRect(x, y, width, height);
         gc.setFill(Color.BLACK);
         gc.fillRect(x + 5, y + height, 10, 10);
         gc.fillRect(x + width - 15, y + height, 10, 10);
+        gc.restore();
     }
 
     @Override
-    public void moveLeft() { x -= speed; }
+    public void moveLeft() {
+        x -= speed;
+        angle = 270;
+    }
+
     @Override
-    public void moveRight() { x += speed; }
+    public void moveRight() {
+        x += speed;
+        angle = 90;
+    }
+
     @Override
-    public void moveForward() { y -= speed; }
+    public void moveForward() {
+        y -= speed;
+        angle = 0;
+    }
+
     @Override
-    public void moveBack() { y += speed; }
+    public void moveBack() {
+        y += speed;
+        angle = 180;
+    }
+
+    private void rotate(GraphicsContext gc, double angle, double px, double py) {
+        Rotate r = new Rotate(angle, px, py);
+        gc.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
+    }
 
     public double getX() { return x; }
     public double getY() { return y; }
--- src/main/java/edu/farmingdale/csc311_assignment3_groupbased/MainApplication.java
+++ src/main/java/edu/farmingdale/csc311_assignment3_groupbased/MainApplication.java
@@ -1,60 +1,23 @@
 package edu.farmingdale.csc311_assignment3_groupbased;
 
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class MainApplication extends Application {
-    private Car car;
-    private GraphicsContext gc;
-    private Image mazeImage;
-
     @Override
-    public void start(Stage primaryStage) {
-        Pane root = new Pane();
-        Canvas canvas = new Canvas(600, 400);
-        gc = canvas.getGraphicsContext2D();
-        root.getChildren().add(canvas);
-
-        mazeImage = new Image(getClass().getResource("/images/maze.png").toExternalForm());
-
-        car = new Car(100, 100);
-
-        Scene scene = new Scene(root, 600, 400, Color.WHITE);
-        scene.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.LEFT) car.moveLeft();
-            if (event.getCode() == KeyCode.RIGHT) car.moveRight();
-            if (event.getCode() == KeyCode.UP) car.moveForward();
-            if (event.getCode() == KeyCode.DOWN) car.moveBack();
-        });
-
-        AnimationTimer timer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                drawScene();
-            }
-        };
-        timer.start();
-
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Maze Game with Car");
-        primaryStage.show();
-    }
-
-    private void drawScene() {
-        gc.clearRect(0, 0, 600, 400);
-        gc.drawImage(mazeImage, 50, 50);
-        car.draw(gc);
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
